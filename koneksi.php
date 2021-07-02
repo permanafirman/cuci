22c619018b45639dfbc46c9f0dfe79b135772725
Firman Permana <firman.dc50@gmail.com>
2021-07-03 03:36:39 +0700

    Set Database

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-    $host = "localhost";
+    $host = "us-cdbr-east-04.cleardb.com";
     $username = "b4859a310bf81d";
     $password = "59c8f505";
     $database = "heroku_3ea3c8ac277ce05";
